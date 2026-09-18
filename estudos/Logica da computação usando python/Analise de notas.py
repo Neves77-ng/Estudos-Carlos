a08701f65bb3e40811ac990f46784970f7e3f47a
Neves77-ng <carloshenriquecclcla@gmail.com>
2026-09-18 11:36:03 -0300

    Att segu analise de notas

--- estudos/Logica da computação usando python/Analise de notas.py
+++ estudos/Logica da computação usando python/Analise de notas.py
@@ -3,13 +3,14 @@ nota2 = float(input("Informe a segunda nota:"))
 nota3 = float(input("Informe a terceira nota:"))
 media = (nota1 + nota2 + nota3) / 3
 
-if media >= 7:
-    print("Aprovado!")
+if nota1 < 0 or nota1 > 10 or nota2 < 0 or nota2 > 10 or nota3  < 0 or nota3 > 10:
+    print("Notas invalidas!!")
+
+elif media >= 7:
+    print("Aprovado! A sua média é {media}!")
 
 elif media >= 5:
-    print("Recuperacão!")
+    print("Recuperacão! A sua média é {media}!")
 
 else:
-    print("Reprovado!")
-
-print(f"sua média é: {media}")
+    print("Reprovado! A sua média é {media}!")
